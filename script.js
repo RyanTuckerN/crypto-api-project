f54b11a123ab19078b197821adb0ecf4e0dc1617
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-07-01 19:24:50 -0400

    added footer, changed to form

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const baseUrl = "http://api.coinlayer.com/";
 const apiKey = "5884c48d58ad121da9dbe3c1965fe573";
 const date = document.querySelector("#date");
 const amount = document.querySelector("#amount");
-const button = document.querySelector("button");
+const form = document.querySelector("form");
 const coins = document.querySelector("#coins");
 const resultsSection = document.querySelector("#results-section");
 
@@ -21,32 +21,42 @@ date.setAttribute("value", "2011-01-01");
 // console.log(date.attributes)
 // console.log(today)
 
+amount.focus() //defaults to start on this input
+
 const setDate = (e) => {
   console.log(e.target.value);
   date.setAttribute("min", "");
   date.setAttribute("value", "");
 
-  if (e.target.value === "BTC") {
-    date.setAttribute("min", "2011-01-01");
-    date.setAttribute("value", "2011-01-01");
-  } else if (e.target.value === "ETH") {
-    date.setAttribute("min", "2015-09-01");
-    date.setAttribute("value", "2015-09-01");
-  } else if (e.target.value === "DOGE") {
-    date.setAttribute("min", "2015-01-01");
-    date.setAttribute("value", "2015-01-01");
-  } else if (e.target.value === "LTC") {
-    date.setAttribute("min", "2013-11-01");
-    date.setAttribute("value", "2013-11-01");
-  } else if (e.target.value === "XMR") {
-    date.setAttribute("min", "2015-04-01");
-    date.setAttribute("value", "2015-04-01");
-  } else if (e.target.value === "XRP") {
-    date.setAttribute("min", "2015-02-01");
-    date.setAttribute("value", "2015-02-01");
-  } else if (e.target.value === "DASH") {
-    date.setAttribute("min", "2014-04-01");
-    date.setAttribute("value", "2014-04-01");
+  switch(e.target.value){
+    case 'BTC':
+      date.setAttribute("min", "2011-01-01");
+      date.setAttribute("value", "2011-01-01");
+      break
+    case 'ETH':
+      date.setAttribute("min", "2015-09-01");
+      date.setAttribute("value", "2015-09-01");
+      break
+    case 'DOGE':
+      date.setAttribute("min", "2015-01-01");
+      date.setAttribute("value", "2015-01-01");
+      break
+    case 'LTC':
+      date.setAttribute("min", "2013-11-01");
+      date.setAttribute("value", "2013-11-01");
+      break
+    case 'XMR':
+      date.setAttribute("min", "2015-04-01");
+      date.setAttribute("value", "2015-04-01");
+      break
+    case 'XRP':
+      date.setAttribute("min", "2015-02-01");
+      date.setAttribute("value", "2015-02-01");
+      break
+    case 'DASH':
+      date.setAttribute("min", "2014-04-01");
+      date.setAttribute("value", "2014-04-01");
+      break 
   }
 };
 
@@ -54,7 +64,8 @@ coins.onchange = setDate;
 
 // *** *** FETCH FUNCTION *** *** //
 
-async function displayResults() {
+async function displayResults(e) {
+  e.preventDefault()
   const investment = amount.value.replace(/[^0-9.]/g, ""); //Regex removes everything except numbers and decimal
   const coin = coins.value;
   const investmentDate = date.value;
@@ -96,21 +107,22 @@ async function displayResults() {
   console.log(modernValue);
 
   // display results
-  resultsSection.innerHTML = `<div class="container d-flex flex-row align-items-center justify-content-center text-center">If you had invested $${investment} in ${coin.toUpperCase()} on ${convertDate(
+  resultsSection.innerHTML = `<div class="container d-flex flex-row align-items-center justify-content-center text-center fade-in">If you had invested $${investment} in ${coin.toUpperCase()} on ${convertDate(
     investmentDate
   )} and HODLed, today you would have </div>
-  <div><img src="./assets/${coin.toLowerCase()}.png"> <span>${coinAmount.toFixed(
+  <div class="fade-in"><img src="./assets/${coin.toLowerCase()}.png"> <span>${coinAmount.toFixed(
     5
   )}</span></div>
-  <p style="font-size: 15px"> which is worth </p>
-  <div><img src="./assets/usd.png"> <span>${toTwoDecimals(
+  <p class="fade-in" style="font-size: 15px"> which is worth </p>
+  <div class="fade-in"><img src="./assets/usd.png"> <span>${toTwoDecimals(
     modernValue.toLocaleString()
   )}</span></div>`;
 }
 
-// RUN THE FUNCTION ON BUTTON CLICK
+// RUN THE FUNCTION ON FORM SUBMISSION
 
-button.addEventListener("click", displayResults);
+
+form.addEventListener("submit", displayResults);
 
 //function that takes a string of numbers and returns string with only two decimal places
 const toTwoDecimals = (numStr) => {
@@ -142,5 +154,3 @@ function convertDate(dateStr) {
 
   return monthArr[month] + " " + day + ", " + year;
 }
-
-
